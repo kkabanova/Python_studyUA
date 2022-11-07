d4d72df835b0efe213eb25c209b896dc330d0635
kristina <16kb.kabanava.k@pdu.by>
2022-11-07 23:18:28 +0300

    Hometask (lesson 2)

---
+++ Package1/first.py
@@ -0,0 +1,1 @@
+print("Kristina Kabanova")
---
+++ Package1/second.py
@@ -0,0 +1,13 @@
+import first
+
+print("\ntype: ", type(first))
+print("id: ", id(first))
+
+age = 23
+print("\nage type: ", type(age))
+print("age id: ", id(age))
+
+# I didn't understand what I should change (type or value)... I changed type to str
+age = "twenty three"
+print("\nnew age type: ", type(age))
+print("new age id: ", id(age))
